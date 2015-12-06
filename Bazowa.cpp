6116b7ba3ea825333223acac958c4e135c9c6431
MrSTALIN <zdenekj23@o2.pl>
2015-12-06 21:37:49 +0100

    Create Bazowa.cpp

---
+++ Bazowa.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "bazowa.h"
+#include <iostream>
+
+
+bazowa::bazowa()
+{
+}
+
+
+bazowa::~bazowa()
+{
+}
+
+void bazowa::pochodze()
+{
+	std::cout << "\t\t\nBazowa\n";
+}
